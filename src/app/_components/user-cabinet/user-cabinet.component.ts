497a6810e973f0bc07086e20d0e817b65d24de2b
Novikov Baruch <salon.bigdaddy@gmail.com>
2020-01-08 13:23:41 +0200

    Structure changes

--- src/app/_components/user-cabinet/user-cabinet.component.ts
+++ src/app/_components/user-cabinet/user-cabinet.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from "../../../_services/user.service";
-import {AuthenticationService} from "../../../_services/authentication.service";
+import {UserService} from "../../_services/user.service";
+import {AuthenticationService} from "../../_services/authentication.service";
 import {catchError} from "rxjs/operators";
 
 @Component({
